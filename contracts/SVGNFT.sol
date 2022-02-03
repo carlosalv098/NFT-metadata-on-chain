2a0527266d98225eafb3883ead5003f44b7ed43c
Carlos Benjamin Alvarez Cruz <carlosalvarez098@gmail.com>
2022-02-03 10:28:54 -0600

    formatTokenURI

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -15,12 +15,28 @@ contract SVGNFT is ERC721URIStorage {
     function create (string memory svg) public {
         _safeMint(msg.sender, tokenId);
         // create imageURI
+        string memory imageURI = svgToImageURI(svg);
         // create tokenURI
+
+        tokenId ++;
     }
 
     function svgToImageURI(string memory svg) public pure returns(string memory imageURI) {
         string memory baseURL = 'data:image/svg+xml;base64,';
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
         imageURI = string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+
+    function formatTokenURI(string memory imageURI) public pure returns(string memory tokenURI) {
+        string memory baseURL = 'data:application/json;base64,';
+        tokenURI = string(abi.encodePacked(
+            baseURL,
+            Base64.encode(bytes(abi.encodePacked(
+                '{"name": "SVG NFT",'
+                '"description": "NFT based on SVG",'
+                '"attributes": "",'
+                '"image": "', imageURI, '"}'
+            ))) 
+        ));
     }
 }
